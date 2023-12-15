6b06a955e31f38f4f095ce2b5e0688c383cfe62b
devebe <dvbirdja@gmail.com>
2023-12-15 12:21:58 +0100

    day 6 solution but prettier

--- day-6/app.js
+++ day-6/app.js
@@ -1,7 +1,15 @@
-const calculateDistance = (raceTime, holdingTime) => raceTime * holdingTime - (holdingTime ** 2);
+// calculateDistance evaluates quadratic function to return distance.
+const calculateDistance = 
+    (raceTime, holdingTime) => raceTime * holdingTime - (holdingTime ** 2);
 
+// findHoldingTimeForMaxDistance uses quadratic rule -b / 2a to 
+// find holdingTime with max distance and rounds it off to the nearest integer.
 const findHoldingTimeForMaxDistance = (raceTime) => Math.round(-raceTime / -2);
 
+// numberOfWaysToBeatRecord evaluates values left and right from holdingTime 
+// that resulted in max distance. Variable numberOfValidSolutions gets 
+// incremented for each time the holdingtime is resulting in a distance higher 
+// than raceDistance.
 const numberOfWaysToBeatRecord = (raceTime, raceDistance) => {
     let holdingTime = findHoldingTimeForMaxDistance(raceTime);
     let left = holdingTime - 1;
@@ -15,14 +23,21 @@ const numberOfWaysToBeatRecord = (raceTime, raceDistance) => {
         numberOfValidSolutions++;
         right++;
     } while (calculateDistance(raceTime, right) > raceDistance);
-    return console.log(numberOfValidSolutions);
+    return numberOfValidSolutions;
 };
 
+// evaluatePuzzleInput multiplies the possible ways to beat the record 
+// for each subarray with raceTime and raceDistance in the array
+const evaluatePuzzleInput = (array) => {
+    let outcomes = [];
+    array.forEach((subarray) => {
+        outcomes.push(numberOfWaysToBeatRecord(subarray[0], subarray[1]));
+    });
+    let answer = outcomes.reduce( (a,b) => a * b )
+    return console.log(answer);
+};
+
+let puzzleInputPart1 = [[40, 219],[81, 1012],[77,1365],[72,1089]];
+evaluatePuzzleInput(puzzleInputPart1);
+
 numberOfWaysToBeatRecord(40817772,219101213651089);
-
-// console.log(calculateDistance(7, 2));
-// console.log(findHoldingTimeForMaxDistance(7));
-// 27 50 22 29
-// 40817772 219101213651089
-
-console.log(27*50*22*29)
